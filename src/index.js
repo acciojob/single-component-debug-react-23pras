4905789cf12b607724850329d218155b7a4a1116
23pras <palprashant465@gmail.com>
2025-08-06 18:15:37 +0530

    code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
+// Render the App component inside the div with id="root"
+ReactDOM.render(<App />, document.getElementById("root"));
